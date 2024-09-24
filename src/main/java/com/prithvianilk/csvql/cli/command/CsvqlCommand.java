c16006e6ed600ad1d4d72b67bd7db01891181e44
prithvianilk <thepirateking3d2y@gmail.com>
2024-09-24 18:40:31 +0530

    Add more context on exception type, handle parser exception

--- src/main/java/com/prithvianilk/csvql/cli/command/CsvqlCommand.java
+++ src/main/java/com/prithvianilk/csvql/cli/command/CsvqlCommand.java
@@ -2,6 +2,7 @@ package com.prithvianilk.csvql.cli.command;
 
 import com.prithvianilk.csvql.executor.QueryExecutionException;
 import com.prithvianilk.csvql.executor.QueryExecutor;
+import com.prithvianilk.csvql.interpreter.parser.ParserException;
 import picocli.CommandLine;
 
 import java.util.concurrent.Callable;
@@ -17,16 +18,28 @@ public class CsvqlCommand implements Callable<Integer> {
             QueryExecutor queryExecutor = new QueryExecutor(query);
             System.out.println(queryExecutor.executeQuery());
             return 0;
+        } catch (ParserException e) {
+            return handleParserException(e);
         } catch (QueryExecutionException e) {
-            System.err.println("Error:");
-            switch (e) {
-                case QueryExecutionException.FileDoesNotExist fileDoesNotExistException -> {
-                    System.err.println("File does not exist: " + fileDoesNotExistException.getFileName());
-                    System.err.println();
-                }
-                default -> System.err.println("Received exception: " + e);
-            }
-            return 1;
+            return handleQueryExecutionException(e);
         }
+    }
+
+    private int handleParserException(ParserException e) {
+        System.err.println("Parsing error: ");
+        throw e;
+    }
+
+    private int handleQueryExecutionException(QueryExecutionException e) {
+        System.err.println("Query execution error:");
+        switch (e) {
+            case QueryExecutionException.FileDoesNotExist fileDoesNotExistException -> {
+                System.err.println("File does not exist: " + fileDoesNotExistException.getFileName());
+                System.err.println();
+            }
+            default -> throw e;
+        }
+
+        return 1;
     }
 }
